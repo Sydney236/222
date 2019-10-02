0dd298df1fd93b5e4a4d8e300a15b703e6496457
Ivan Sidenko <ivansidenkoi28@gmail.com>
2019-10-02 20:53:48 +0300

    initial project version

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task5b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки");
+            double x, y;
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Выбранная точка ({x};{y})");
+            if ((x*x+y*y<1) && (x>=0))
+            {
+                Console.WriteLine("Выбранная точка входит в заштрихованную область");
+            }
+            else
+            {
+                Console.WriteLine("Выбранная точка не входит в заштрихованную область");
+            }
+        }
+    }
+}
