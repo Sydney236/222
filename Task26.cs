fdf6e152784eee9ea96c9de7f2e8db8d102d3425
Ivan Sidenko <ivansidenkoi28@gmail.com>
2019-10-20 19:48:41 +0300

    Initial

---
+++ Task26.cs
@@ -0,0 +1,71 @@
+﻿/**
+
+* @author Ivan Sidenko
+
+* 11-912
+
+* Task 26
+
+*/
+using System;
+
+namespace Task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк матрицы");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов матрицы");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[m, n];
+            Console.WriteLine("Заполните эту матрицу");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Полученная матрица:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{a[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            int max = 0;
+            int savei = 0;
+            int counti = 1;
+            for (int j = 0; j < n; j++)
+            {
+                int savej = j;
+                int smax = 0;
+                int count = 0;
+                for (int i = 0; j < n && i < m; i++)
+                {
+                    count++;
+                    smax += a[i, j];
+                    j++;
+                    savei = i;
+                }
+                j = 0;
+                if (count < m && count < n)
+                {
+                    for (int i = m - counti; i < m; i++) 
+                    {
+                        smax += a[i, j];
+                        j++;
+                    }
+                    counti++;
+                }
+                if (smax > max) max = smax;
+                j = savej;
+            }
+            Console.Write($"Ответ: {max}");
+        }
+    }
+}
---
+++ Task27.cs
@@ -0,0 +1,84 @@
+﻿/**
+
+* @author Ivan Sidenko
+
+* 11-912
+
+* Task 27
+
+*/
+using System;
+
+namespace Task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк матрицы");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов матрицы");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[m, n];
+            Console.WriteLine("Заполните эту матрицу");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Полученная матрица:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{a[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Введите количество столбцов второй матрицы");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int[,] b = new int[n, p];
+            Console.WriteLine("Заполните эту матрицу");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    b[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Полученная матрица:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    Console.Write($"{b[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            int[,] c = new int[m, p];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    int s = 0;
+                    for (int w = 0; w < n; w++)
+                    {
+                        s += a[i, w] * b[w, j];
+                    }
+                    c[i, j] = s;
+                }
+            }
+            Console.WriteLine("Результат умножения матриц:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    Console.Write($"{c[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
